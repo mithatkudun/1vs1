d4cd82ae0c0921659b17323d4e12ccd06195790f
Mithat KUDUN <mithatkudun@gmail.com>
2023-06-05 14:05:49 +0300

    First

---
+++ Assets/Scripts/MainMenuDisplay.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.SceneManagement;
+public class MainMenuDisplay : MonoBehaviour
+{
+    [SerializeField] private string gameplaySceneName = "Gameplay";
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.SceneManager.LoadScene(gameplaySceneName, LoadSceneMode.Single);
+    }
+    public void StartServer()
+    {
+        NetworkManager.Singleton.StartServer();
+    }
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+}
