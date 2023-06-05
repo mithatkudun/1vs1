6b53fe690e21bfc2420d8a39beb7f7bb4abe6aff
Mithat KUDUN <mithatkudun@gmail.com>
2023-06-05 14:46:25 +0300

    Second

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Characters/Character")]
+public class Character : ScriptableObject
+{
+
+}
--- Assets/Scripts/CharacterSelectDisplay.cs
+++ Assets/Scripts/CharacterSelectDisplay.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Unity.Netcode;
+using System;
 
-public class CharacterSelectDisplay : MonoBehaviour
+public class CharacterSelectDisplay : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private NetworkList<CharacterSelectState> players;
+
+    private void Awake()
     {
+        players = new NetworkList<CharacterSelectState>();
+    }
+    public override void OnNetworkSpawn()
+    {
+        if(IsServer)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback += HandleClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback += HandleClientDisconnected;
         
+            foreach(NetworkClient client in NetworkManager.Singleton.ConnectedClientsList)
+            {
+                HandleClientConnected(client.ClientId);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public override void OnNetworkDespawn()
     {
-        
+        if(IsServer)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback -= HandleClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback -= HandleClientDisconnected;
+        }
+    }
+
+    private void HandleClientConnected(ulong clientId)
+    {
+        players.Add(new CharacterSelectState(clientId));
+    }
+    private void HandleClientDisconnected(ulong clientId)
+    {
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (players[i].ClientId == clientId)
+            {
+                players.RemoveAt(i);
+                break;
+            }
+        }
     }
 }
